15af25902a7e22f46089483c5eba1d66c7957536
sally ayoub <82056759+SallyAyoub@users.noreply.github.com>
2022-07-07 23:26:10 +0300

    Update Task-1.py

--- Task-1.py
+++ Task-1.py
@@ -1,55 +1,100 @@
 import json
 
 
-def print_total_score(score, number_questions):
-    print("Your Total Score is :" + " " + str(score) + "/" + str(number_questions) + "\n")
+def print_total_score(score: int, number_questions: int) -> None:
+    """
+
+    Args:
+        score (int): the number of questions the user answered right
+        number_questions (int): the total number of questions
+
+    Returns:
+        None
+
+    """
+    print(f"your total score is {score} / {number_questions}")
 
 
-def read_option(option):
+def read_file() -> dict:
+    """
+    Args:
+        None
+
+    Returns:
+        dictionary that contains the json file data
+
+    """
+    with open('quiz.json', 'r') as json_file:
+        json_dictionary_data = json.load(json_file)
+    return json_dictionary_data
+
+
+def read_option(option: int) -> None:
+    """
+
+    Args:
+        option (int): the numerical option of the category the user entered
+    Returns:
+        None
+    """
+    try:
+        category_of_option = categorise[option]
+        get_questions(category_of_option)
+    except IndexError:
+        print("The option you Entered isn't valid please try again")
+
+
+def get_questions(selected_category: str) -> None:
+    """
+    Args:
+        selected_category (str): the category the user selected to take its quiz
+    Returns:
+        None
+    """
     total_score = 0
-    if option == 'x':
-        print(" Exiting the quiz \n")
-        exit()
-    else:
-        option = int(option)
-        category_of_option = list_of_categories[option - 1]
-        option_dictionary = json_dictionary['quiz'].get(category_of_option, -1)
-        if option_dictionary == -1:
-            print(" This Option doesn't exist \n")
-            exit()
-        else:
-            option_tuple = (option_dictionary.items())
-            option_list = list(option_tuple)
-            number_questions = 0
-            for question in option_list:
-                print(question[0])
-                number_questions += 1
-                print("The Question is :")
-                print(question[1].get('question'))
-                print("The Options are :")
-                options = (question[1].get('options'))
-                for j in range(0, len(options)):
-                    print(options[j])
-                print("                                                         ")
-                guess = str(input("Please Enter your guessing: \n "))
-                if guess.capitalize() == question[1].get('answer').capitalize():
-                    total_score += 1
-            print_total_score(total_score, number_questions)
+    option_dictionary = json_dictionary['quiz'][selected_category]
+    for questionKey, questions in option_dictionary.items():
+        print(f"{questionKey}")
+        for key, value in questions.items():
+            if key == 'question':
+                print(f"{key}:")
+                print(f"{value}:")
+            elif key == 'options':
+                options_list = value
+                for option in range(0, len(value)):
+                    print(f"{option + 1}) {value[option]}")
+            else:
+                question_score = guess_answer(value, options_list)
+                total_score += question_score
+    print_total_score(total_score, len(option_dictionary))
 
 
-with open('quiz.json', 'r') as json_file:
-    json_dictionary = json.load(json_file)
+def guess_answer(answer: str, options_list: list) -> int:
+    """
+    Args:
+        answer (str): the question's correct answer
+        options_list (list): the answers options
+    Returns:
+        score (int): the score the user got in the question
+    """
+    score = 0
+    guess = int(input("Please Enter your guessing:"))
+    try:
+        if answer == options_list[guess-1]:
+            score += 1
+    except IndexError:
+        print("The option you Entered isn't valid please try again")
+    return score
 
-categorise = list(json_dictionary['quiz'].keys())
-index = 0
-print(" This Quiz has questions for the categories: \n")
-list_of_categories = []
 
-for i in categorise:
-    list_of_categories.insert(index, i)
-    index += 1
-    print(str(index) + "-" + " " + i + "\n")
+if __name__ == '__main__':
+    json_dictionary = read_file()
+    categorise = list(json_dictionary['quiz'].keys())
 
-while True:
-    category = str(input("Please enter your Option:  \n"))
-    read_option(category)
+    while True:
+        index = 0
+        print("This Quiz has questions for the categories: ")
+        for i in range(0, len(categorise)):
+            print(f"{str(i + 1)}- {categorise[i]}")
+        category = int(input("Please enter your Option: "))
+        read_option(category - 1)
