345c7ebf12c2e2b138e08efe1749b6124401f505
sally ayoub <82056759+SallyAyoub@users.noreply.github.com>
2022-07-14 16:17:06 +0300

    Update Task-1.py

--- Task-1.py
+++ Task-1.py
@@ -1,28 +1,14 @@
 import json
 
 
-def print_total_score(score: int, number_questions: int) -> None:
-    """
-
-    Args:
-        score (int): the number of questions the user answered right
-        number_questions (int): the total number of questions
-
-    Returns:
-        None
-
-    """
-    print(f"your total score is {score} / {number_questions}")
-
-
 def read_file() -> dict:
     """
+    This function reads the contents of the json file and returns them in a dictionary
     Args:
         None
 
     Returns:
         dictionary that contains the json file data
-
     """
     with open('quiz.json', 'r') as json_file:
         json_dictionary_data = json.load(json_file)
@@ -31,11 +17,13 @@ def read_file() -> dict:
 
 def read_option(option: int) -> None:
     """
-
+    This function reads the category option the user entered and calls for the get_questions function
     Args:
         option (int): the numerical option of the category the user entered
     Returns:
         None
+    Raises:
+        IndexError: if the input option is out of bound
     """
     try:
         category_of_option = categorise[option]
@@ -46,6 +34,8 @@ def read_option(option: int) -> None:
 
 def get_questions(selected_category: str) -> None:
     """
+    This function loads the questions for the chosen category by the user to the terminal and for each question it calls
+     the guess_answer function ,then it calls print_total_score function
     Args:
         selected_category (str): the category the user selected to take its quiz
     Returns:
@@ -71,20 +61,45 @@ def get_questions(selected_category: str) -> None:
 
 def guess_answer(answer: str, options_list: list) -> int:
     """
+    This function prompts the user to enter the guessing option and checks whether their guessing is right or not
     Args:
         answer (str): the question's correct answer
         options_list (list): the answers options
     Returns:
-        score (int): the score the user got in the question
+         score (int): the score the user got in the question
+    Raises:
+        ValueError: if the user enters a non numerical option
+        IndexError: if the user option is out of bound
     """
     score = 0
-    guess = int(input("Please Enter your guessing:"))
     try:
-        if answer == options_list[guess-1]:
+        guess = int(input("Please Enter your guessing:"))
+        if guess <= 0:
+            print("The option you Entered isn't valid please try again")
+            return score
+
+    except ValueError:
+        print("The option you Entered isn't valid please try again")
+        return score
+    try:
+        if answer == options_list[guess - 1]:
             score += 1
     except IndexError:
         print("The option you Entered isn't valid please try again")
     return score
+
+
+def print_total_score(score: int, number_questions: int) -> None:
+    """
+    This function prints the total score of the quiz by dividing the questions score by their number
+    Args:
+        score (int): the number of questions the user answered right
+        number_questions (int): the total number of questions
+
+    Returns:
+        None
+    """
+    print(f"your total score is {score} / {number_questions}")
 
 
 if __name__ == '__main__':
@@ -96,5 +111,5 @@ if __name__ == '__main__':
         print("This Quiz has questions for the categories: ")
         for i in range(0, len(categorise)):
             print(f"{str(i + 1)}- {categorise[i]}")
-        category = int(input("Please enter your Option: "))
+        category = int(input("Please enter your Option : \n"))
         read_option(category - 1)
